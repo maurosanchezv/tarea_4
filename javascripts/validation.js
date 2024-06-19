d21824fde9231c0c764373ba5526adea8b43d7f4
maurosanchezv <minecraftultimatum39@gmail.com>
2024-06-19 17:42:09 -0400

    tarea 4

---
+++ javascripts/validation.js
@@ -0,0 +1,23 @@
+function emailValidation() {
+  const form = document.getElementById("form");
+  const emailComfirmField = document.getElementById("email_confirm");
+  const element = document.createElement("p");
+  const message = document.createTextNode("Correo electrónico no coincidente.");
+  const contentField = document.getElementById("content_0");
+  element.appendChild(message);
+  element.setAttribute("id", "alert");
+  element.classList.add("alert_color");
+  emailComfirmField.addEventListener("input", (e) => {
+    if (form.email.value !== form.email_confirm.value) {
+      if (!document.getElementById("alert")) {
+        contentField.parentNode.insertBefore(element, contentField);
+        emailComfirmField.classList.add("alert_bg");
+      }
+    } else {
+      emailComfirmField.classList.remove("alert_bg");
+      element.parentNode.removeChild(element);
+    }
+  });
+}
+
+window.onload = emailValidation;
